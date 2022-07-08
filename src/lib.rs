12c2215c083cc0e80768cfe3d7a17dd7759eb723
eugenesvk <eugenesvk@users.noreply.github.com>
2022-07-09 00:20:19 +0700

    add dummy code to the main lib
    to be used by extension pages

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,24 @@
-use wasm_bindgen::prelude::*;
+#![cfg_attr(debug_assertions, allow(dead_code, unused_imports, unused_variables))]
+#![warn(unreachable_pub)]
 
-#[wasm_bindgen]
-extern "C" {
-  fn alert(s: &str);
-}
+use wasm_bindgen::prelude::	*;
+use web_sys::              	console;
 
-#[wasm_bindgen(start)]
-pub fn main_js() -> Result<(), JsValue> {
-  alert(&format!("Hello!"));
-  Ok(())
-}
+pub mod style;
+
+/// print to console.log, but only during debug builds: `[file:line]  log message`
+#[macro_export] macro_rules! dbglog { ($($args:tt)*) => {
+  #[cfg(    debug_assertions)] {
+  $crate::log (std::format!("[{}:{}]  {}", std::file!(), std::line!(), std::format!($($args)*))); }
+  #[cfg(not(debug_assertions))] { ($($args)*) } };}
+/// print to console.log: `[file:line]  log message`
+#[macro_export] macro_rules! log    { ($($args:tt)*) => {
+  $crate::log (std::format!("[{}:{}]  {}", std::file!(), std::line!(), std::format!($($args)*))); };}
+// #[macro_export] macro_rules! info { ($($args:tt)*) => {
+  // $crate::info($crate::pretty_time()     , std::file!(), std::line!(), std::format!($($args)*));  };}
+/// print to console.warn: `[file:line]  log message`
+#[macro_export] macro_rules! warn   { ($($args:tt)*) => {
+  $crate::warn(std::format!("[{}:{}]  {}", std::file!(), std::line!(), std::format!($($args)*))); };}
+#[inline] pub fn log (s:String) {/*! console.log()  */ console::log_1  (&JsValue::from(s)); }
+#[inline] pub fn warn(s:String) {/*! console.warn() */ console::warn_1 (&JsValue::from(s)); }
+#[inline] pub fn err (s:String) {/*! console.error()*/ console::error_1(&JsValue::from(s)); }
