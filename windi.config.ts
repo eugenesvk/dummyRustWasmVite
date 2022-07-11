b88e820cbae427d92e244dad9471e51ef657510d
eugenesvk <eugenesvk@users.noreply.github.com>
2022-07-11 19:00:05 +0700

    windicss: add config

---
+++ windi.config.ts
@@ -0,0 +1,12 @@
+import { resolve }     	from 'path'
+import { defineConfig }	from 'windicss/helpers'
+
+export default defineConfig({
+  darkMode: 'class',
+  attributify: true, // windicss.org/posts/v30.html#attributify-mode
+  extract: {
+    include: [
+      resolve(__dirname, 'src/**/*.{tsx,html}'),
+    ],
+  },
+})
