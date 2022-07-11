8fb25c41b1d5c122b68b227821d763690e5fc7f6
eugenesvk <eugenesvk@users.noreply.github.com>
2022-07-11 18:59:44 +0700

    windicss: add the old options app to generate windicss

---
+++ src/options/Options.tsx
@@ -0,0 +1,11 @@
+export default function App () {
+  return (
+  <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
+  <img src={''} class="icon-btn mx-2 text-2xl"/>
+  <p class="mt-2 opacity-50"></p>
+  <input value={''} oninput={''}
+    class="border border-gray-400 rounded px-2 py-1 mt-2" />
+  <div class="mt-4"><IconPixelarticonsZap class="align-middle"/>
+  </div>
+ </main>
+);}
---
+++ src/options/main.ts
@@ -0,0 +1,5 @@
+import App	from './Options'
+import    	     '../styles'
+
+document.getElementById('app').textContent = '';
+render(App, document.getElementById('app'));
