fdecf09b1d957c6fa8269277be9cf3f9b0cf47e8
eugenesvk <eugenesvk@users.noreply.github.com>
2022-07-11 18:57:04 +0700

    windicss: add the old popup app to generate windicss

---
+++ src/popup/Popup.tsx
@@ -0,0 +1,9 @@
+import Logo	from '../components/Logo'
+export default function App () {
+  return (
+    <main class="w-[300px] px-4 py-5 text-center text-gray-700">
+    <p class="mt-2 opacity-50"></p>
+    <button class="btn mt-2"></button>
+    <div class="mt-2"><span class="opacity-50"></span></div>
+  </main>
+);}
---
+++ src/popup/main.ts
@@ -0,0 +1,5 @@
+import App	from './Popup'
+import    	     '../styles'
+
+document.getElementById('app').textContent = '';
+render(App, document.getElementById('app'));
