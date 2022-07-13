b93a81c0c8f4bd3e3d90bbc8cfb070b6ba37dcdd
eugenesvk <eugenesvk@users.noreply.github.com>
2022-07-14 01:39:10 +0700

    styles: remove @apply inlining
    move styles to individual elements to ensure Rust styles have an effect

--- src/options/Options.tsx
+++ src/options/Options.tsx
@@ -1,7 +1,9 @@
 export default function App () {
   return (
   <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
-  <img src={''} class="icon-btn mx-2 text-2xl"/>
+  <img src={''} class="icon-btn mx-2 text-2xl inline-block cursor-pointer select-none
+    opacity-75 transition duration-200 ease-in-out
+    hover:opacity-100 hover:text-teal-600"/>
   <p class="mt-2 opacity-50"></p>
   <input value={''} oninput={''}
     class="border border-gray-400 rounded px-2 py-1 mt-2" />
--- src/popup/Popup.tsx
+++ src/popup/Popup.tsx
@@ -3,7 +3,7 @@ export default function App () {
   return (
     <main class="w-[300px] px-4 py-5 text-center text-gray-700">
     <p class="mt-2 opacity-50"></p>
-    <button class="btn mt-2"></button>
+    <button class="btn mt-2 px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50"></button>
     <div class="mt-2"><span class="opacity-50"></span></div>
   </main>
 );}
