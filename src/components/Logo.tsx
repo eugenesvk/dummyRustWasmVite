12e9f71c5cb018b0e7fffcff0dec6566d25c7c77
eugenesvk <eugenesvk@users.noreply.github.com>
2022-07-18 18:56:29 +0700

    windicss: add Logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,8 @@
+export default function Logo () {
+  return (
+    <a class="icon-btn mx-2 text-2xl inline-block cursor-pointer select-none
+      opacity-75 transition duration-200 ease-in-out
+      hover:opacity-100 hover:text-teal-600" rel="noreferrer" href="https://github.com/antfu/vitesse-webext" target="_blank" title="GitHub">
+      <IconPixelarticonsPower/>
+    </a>
+);}
