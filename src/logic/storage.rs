474bd65a5975c4ae753155bf560157afb115a095
eugenesvk <eugenesvk@users.noreply.github.com>
2022-07-30 15:59:28 +0700

    _tmp_ convert storage logic to Rust

--- src/logic/storage.rs
+++ src/logic/storage.rs
@@ -5,16 +5,21 @@
 use wasm_bindgen::prelude::                	*;
 use crate::                                	log;
 use crate::composables::use_storage_local::	{use_storage_local_fn, storage_local_api};
+use wasm_bindgen_futures::                 	{JsFuture, spawn_local};
 
 #[wasm_bindgen]
 // pub async fn storage_logic() -> i32 {
 pub fn storage_logic() -> i32 {
   let aaa = use_storage_local_fn();
   let bbb = storage_local_api();
-  let ddd = async { aaa.await + bbb.await };
+  // let ddd = async { aaa.await + bbb.await };
 
   use futures::executor::block_on;
-  block_on(ddd)
+  // aaa.await;
+  // block_on(aaa); // 'condvar wait not supported' panick, can't block in a browser (Async in the browser has to use the browser's executor, you can't bring your own, and you can't ever block on async functions)
+  // spawn_local(async {aaa.await; ()}); // works on next tick
+  spawn_local(async {aaa.await; ()}); // works on next tick
+  3
 }
 
 /* storage.ts
