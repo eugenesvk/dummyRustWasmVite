ea99fac88375740616764128ce730916aac700f6
eugenesvk <eugenesvk@users.noreply.github.com>
2022-07-30 15:01:18 +0700

    storage: convert local_storage to WebExt

--- src/storage_web_ext/src/local_storage.rs
+++ src/storage_web_ext/src/local_storage.rs
@@ -2,16 +2,14 @@ use wasm_bindgen::UnwrapThrowExt;
 
 use crate::Storage;
 
-/// Provides API to deal with `localStorage`
+/// Provides API to deal with `storage.local`
 #[derive(Debug)]
 pub struct LocalStorage;
 
 impl Storage for LocalStorage {
-    fn raw() -> web_sys::Storage {
-        web_sys::window()
-            .expect_throw("no window")
-            .local_storage()
-            .expect_throw("failed to get local_storage")
-            .expect_throw("no local storage")
-    }
+  fn raw() -> web_extensions_sys::StorageCommon {
+    web_extensions_sys::browser
+      .storage()
+      .local()
+  }
 }
