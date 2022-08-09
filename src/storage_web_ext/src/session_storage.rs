cf3e787b8c0de6bf28384021135d3cd2aa6732e8
eugenesvk <eugenesvk@users.noreply.github.com>
2022-08-10 00:15:05 +0700

    ___storage: convert session_storage to WebExt

--- src/storage_web_ext/src/session_storage.rs
+++ src/storage_web_ext/src/session_storage.rs
@@ -1,10 +1,8 @@
-use wasm_bindgen::UnwrapThrowExt;
-
 use crate::Storage;
 
-/// Provides API to deal with `sessionStorage`
+/// Provides API to deal with `storage.session`
 #[derive(Debug)]
-pub struct SessionStorage;
+pub struct       SessionStorage;
 
 impl Storage for SessionStorage {
   fn raw() -> web_extensions_sys::StorageArea {
